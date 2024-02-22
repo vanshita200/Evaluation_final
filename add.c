08aa284143593b59d6b49a78e568fc6b91977a65
petrifiedsage <antonymanjila1@gmail.com>
2024-02-22 15:24:27 +0530

    cham

--- add.c
+++ add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sub ;
+    int num1, num2, sum ;
     
     printf("enter the first num: ");
     scanf("%d", &num1);
@@ -9,8 +9,8 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    sum = num1 - num2;
-    printf("The sum of %d and %d is %d.", num1, num2, sub);
+    sum = num1 + num2;
+    printf("The sum of %d and %d is %d.", num1, num2, sum);
     
     return 0;
 }
