b07c021a820f8a9cd6520e152f1edeee44174c9f
siya <156298375+siyakri@users.noreply.github.com>
2024-02-22 14:53:43 +0530

    Update add.c
    substraction of the no cn be done using it

--- add.c
+++ add.c
@@ -3,10 +3,10 @@
 int main() {
     int num1, num2, sum ;
     
-    printf("enter the first num: ");
+    printf("Enter the first no: ");
     scanf("%d", &num1);
     
-    printf("Enter the second number: ");
+    printf("Enter the second no: ");
     scanf("%d", &num2);
     
     sum = num1 + num2;
